c601a07db946676c00a33f81e7fb40250d851620
Masiar Novine <novinefarahb@uni-potsdam.de>
2024-03-25 20:05:53 +0100

    feat(tutorial): complete step 1

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,5 @@
 'use client';
-import { Button } from '@carbon/react';
 import LandingPage from './home/page';
-import RepoPage from './repos/page';
-import Providers from './providers';
 
 export default function Page() {
   return <LandingPage />;
